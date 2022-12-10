471c1e95e881cc5371258d2234851536ef8c55d8
ehis2emma <ehis2emma@yahoo.com>
2022-12-09 19:52:20 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+main void
+int main(void)
+{
+	printf("Size of  char: %ibyte(s)\n", sizeof(char));
+	printf("Size of  int: %ibyte(s)\n", sizeof(int));
+	printf("Size of  long int: %ibyte(s)\n", sizeof(long int));
+	printf("Size of  long long int: %ibyte(s)\n", sizeof(long long int));
+	printf("Size of float: %ibyte(s)\n", sizeof(float));
+	printf("Size of  long long int: %ibyte(s)\n", sizeof(long long int));
+	printf("Size of  float: %ibyte(s)\n", sizeof(float));
+	return (0);
+}
