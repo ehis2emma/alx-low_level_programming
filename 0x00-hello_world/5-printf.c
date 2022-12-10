044985da798668cb4086b5b33baae0f99f8485d1
ehis2emma <ehis2emma@yahoo.com>
2022-12-09 19:33:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio>
 
 /**
- * main Entry
- * Return: Always o
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcomeis a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
