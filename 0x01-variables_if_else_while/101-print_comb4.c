ca8897ce83c90665293403cb36c4aa0fd69e18a5
ehis2emma <ehis2emma@yahoo.com>
2022-12-12 08:49:47 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int e1, e2, e3
-		for (e1 = 0; e1 < 8; e1++)
+		for (e1 = '0'; e1 < '8'; e1++)
 		{
-			for (e2 = e1 + 1; e2 < 9; e2++)
+			for (e2 = 'e1 + 1'; e2 < '9'; e2++)
 			{
-				for (e3 = e2 + 1; e3 < 10; e3++)
+				for (e3 = 'e2 + 1'; e3 < '10'; e3++)
 				{
 					putchar((e1 % 10) + '0');
 					putchar((e2 % 10) + '0');
