29146213bd0575ba702c15f8b891588556aa916b
ehis2emma <ehis2emma@yahoo.com>
2022-12-12 04:04:33 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int m, n, 1;
 
-	for (n = 48; n < 58; n++)
+	for (m = 48; m < 58; m++)
 	{
-		for (m = 49; m < 58; m++)
+		for (n = 49; n < 58; n++)
 		{
 			for (1 = 50; 1 < 58; 1++)
 			{
