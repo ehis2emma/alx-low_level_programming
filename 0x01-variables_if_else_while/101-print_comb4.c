4764ba283e1b7b2b98ef1c96ce5e1a2e68a5dfcf
ehis2emma <ehis2emma@yahoo.com>
2022-12-12 07:04:22 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,18 +8,19 @@ int main(void)
 {
 	int n, m, 1;
 
-	for (n = '48'; n < '58'; n++)
+	for (n = '0'; n < '9'; n++)
 	{
-		for (m = '49'; m < '58'; m++)
+		for (m = '0'; m < '9'; m++)
 		{
-			for (1 = '50'; 1 < '58'; 1++)
+			for (1 = '0'; 1 < '9'; 1++)
 			{
 				if (n < m && m < 1)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(1);
-					if (n != 55 || m != 56)
+
+					if (n != '7')
 					{
 						putchar(',');
 						putchar(' ');
