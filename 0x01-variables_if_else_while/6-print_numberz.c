ec07dfbd71e87eaf98eed3018c90015766d2ee77
ehis2emma <ehis2emma@yahoo.com>
2022-12-11 19:06:27 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,13 @@
  * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
-init main(void)
+int main(void)
 {
 	int n;
 
 	for (n = 48; n < 58; n++)
 	{
-		putchar(n)
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
