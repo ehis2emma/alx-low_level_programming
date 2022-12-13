2032531da413424eef27813374d8e7b571519d71
ehis2emma <ehis2emma@yahoo.com>
2022-12-12 17:32:45 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry pont
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, k, j;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (a + b) && k >= a) || a < k)
+				{
+					purchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (a + b + k + j == 227 && a == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
