0aaf2e87e04d92466beda74251049047a12a3317
ehis2emma <ehis2emma@yahoo.com>
2022-12-16 17:53:46 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putcher('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_puthcar('r');
+	_putchar('\n');
+
+return (0);
+}
