1c3fd511abbb555bad7f8fe6a6cec4ec719389e7
ehis2emma <ehis2emma@yahoo.com>
2022-12-18 17:31:26 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar01.c
+++ 0x02-functions_nested_loops/0-putchar01.c
@@ -6,17 +6,15 @@ include "main.h"
 */
 int main(void)
 {
-	char text[10] = "_putchar";
-	int i = 0;
-
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-
+	_putchar(',');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
-
-	return (0);
+return (0);
 }
