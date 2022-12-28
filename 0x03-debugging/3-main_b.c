aaab18305d69872c57334976b20997c09103bea7
ehis2emma <ehis2emma@yahoo.com>
2022-12-28 07:11:35 -0800

    3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -2,11 +2,10 @@
 #include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
-
+  *main - days left in a year plus leap years
+  *
+  *Return: 0
+  */
 int main(void)
 {
 	int month;
@@ -18,9 +17,7 @@ int main(void)
 	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
 	day = convert_day(month, day);
-
 	print_remaining_days(month, day, year);
 
 	return (0);
