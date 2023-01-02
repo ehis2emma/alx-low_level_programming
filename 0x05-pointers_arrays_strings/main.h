864e357e3a6919e25ed337edde4935e37bad45f8
ehis2emma <ehis2emma@yahoo.com>
2023-01-01 21:43:26 -0800

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unistd.h>
 
-int _putchar(char c);
+/*
+ * File: main.h
+ * Description: contains all function prototype used in this project
+ */
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,5 +18,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
+
