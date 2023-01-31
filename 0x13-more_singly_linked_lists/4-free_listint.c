2a65a138f9e2d7eb4f50a44a1b9888caa81ddb0e
ehis2emma <ehis2emma@yahoo.com>
2023-01-31 12:47:45 -0800

    singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,41 +1,21 @@
-/*
- * File: 3-add_nodeint_end.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
 
 /**
- * add_nodeint_end - Adds a new node at the
- *                   end of a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @n: The integer for the new node to contain.
+ * free_listint - free list
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new element.
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+ * @head: pointer to the first node of the list
+ *
+ * Return: nothing
+*/
+void free_listint(listint_t *head)
 {
-	listint_t *new, *last;
+	listint_t *current;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-
-	else
+	/*iterate through each node in list*/
+	while ((current = head) != NULL)
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		/*set head as the next node*/
+		head = head->next;
+		free(current);
 	}
-
-	return (*head);
 }
